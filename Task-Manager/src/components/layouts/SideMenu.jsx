1f7c06e01af26b38914284519f3488345ac1cdb4
Veronika <sanchezveka3@gmail.com>
2025-08-01 03:02:39 +0200

    feat:  create add members to tasks

--- Task-Manager/src/components/layouts/SideMenu.jsx
+++ Task-Manager/src/components/layouts/SideMenu.jsx
@@ -58,11 +58,11 @@ const SideMenu = ({activeMenu}) => {
         {sideMenuData.map((item, index) => (
             <button
             key={`menu_${index}`}
-            className={`w-full flex items-center gap-4 text-[15px] ${
-                activeMenu == item.label
+            className={`w-full flex items-center gap-4 text-[15px] py-3 px-6 mb-3 cursor-pointer font-medium transition-all duration-200 ${
+                activeMenu === item.label
                 ? "text-primary bg-linear-to-r from-blue-50/40 to-blue-100/500 border-r-3"
-                : ""
-            } py-3 px-6 mb-3 cursor-pointer`}
+                : "text-gray-700 hover:bg-blue-50 hover:text-primary"
+            } `}
             onClick={() => handleClick(item.path)}
             >
                 <item.icon className="text-xl" />
--- Task-Manager/src/pages/Admin/Dashboard.jsx
+++ Task-Manager/src/pages/Admin/Dashboard.jsx
@@ -11,6 +11,10 @@ import { addThousandsSeparator } from '../../utils/helper'
 import InfoCard from '../../components/cards/InfoCard'
 import { LuArrowRight } from 'react-icons/lu'
 import TaskListTable from '../../components/layouts/TaskListTable'
+import CustomPieChar from '../../components/charts/CustomPieChar'
+import CustomBarChar from '../../components/charts/CustomBarChar'
+
+const COLORS = ["#8D51FF", "#00B8DB", "#7BCE00"]
 
 
 const Dashboard = () => {
@@ -24,6 +28,32 @@ const Dashboard = () => {
   const [pieChartData, setPieChartData] = useState([]);
   const [barChartData, setBarChartData] = useState([]);
 
+
+  //datos de la grafica
+  const prepareChartData = (data) => {
+    const taskDistribution = data?.taskDistribution || null;
+    const taskPriorityLevels = data?.taskPriorityLevels || null;
+
+    const taskDistributionData = [
+      { status: "Pending", count: taskDistribution?.Pending || 0 },
+      { status: "In Progress", count: taskDistribution?.InProgress || 0 },
+      { status: "Completed", count: taskDistribution?.Completed || 0 },
+    ];
+
+    setPieChartData(taskDistributionData);
+
+    const priorityLevelData = [
+      { priority: "Low", count: taskPriorityLevels?.Low || 0 },
+      { priority: "Medium", count: taskPriorityLevels?.Medium || 0 },
+      { priority: "High", count: taskPriorityLevels?.High || 0 },
+    ];
+
+    setBarChartData(priorityLevelData);
+
+
+  }
+
+
   const getDashboardData = async () => {
     try {
       const response = await axiosInstance.get(
@@ -31,6 +61,7 @@ const Dashboard = () => {
       );
       if(response.data){
         setDashboardData(response.data);
+        prepareChartData(response.data?.charts || null)
       }
     } catch (error) {
       console.error("Error al recuperar los usuarios:", error);
@@ -47,6 +78,8 @@ const Dashboard = () => {
     return () => {}
   }, []);
   
+  console.log("barChartData:", barChartData);
+
 
   return <DashboardLayout activeMenu="Dashboard">
     <div className="card my-5">
@@ -93,6 +126,22 @@ const Dashboard = () => {
         />
       </div>
     </div>
+
+        <div>
+          <div className="card">
+            <div className="flex items-center justify-between">
+              <h5 className="font-medium">Distribución de Tareas</h5>
+            </div>
+
+            <CustomPieChar
+            data={pieChartData}
+            colors={COLORS}
+            />
+          </div>
+        </div>
+
+
+
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-4 md:my-6">
           <div className="md:col-span-2">
